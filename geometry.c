171df997288f9f2d9f91e4d01a3f7b2561c92856
SofyaMogutova <smogutova1234@gmail.com>
2021-03-13 11:30:49 +0700

    Add edited code

--- geometry.c
+++ geometry.c
@@ -7,21 +7,22 @@
 
 int main()
 {
-	char figure[7];
-	double x,y,rad;
-	char geometry;
-	char cir[]="circle";
-	printf("Введите фигуру в формате WKT:");
-	scanf("%s", figure);
-	scanf("%*[( ] %lf %lf %*[, ] %lf %*[) ]",&x,&y,&rad);
-    if (strncmp(cir,figure,6)!=0) {
-    	printf("Error at column 0: expected 'circle'\n");
+    char figure[7];
+    double x, y, rad;
+    char geometry;
+    char cir[] = "circle";
+    printf("Введите фигуру в формате WKT:");
+    scanf("%s", figure);
+    scanf("%*[( ] %lf %lf %*[, ] %lf %*[) ]", &x, &y, &rad);
+    if (strncmp(cir, figure, 6) != 0) {
+        printf("Error at column 0: expected 'circle'\n");
     }
-    if ((x==(int)x) || (y==(int)y) || (rad==(int)rad)) {
-        printf ("Error at column 7: expected '<double>'\n");
+    if ((x == (int)x) || (y == (int)y) || (rad == (int)rad)) {
+
+        printf("Error at column 7: expected '<double>'\n");
     }
-    if (rad<=0) {
-        printf ("Error: data entered incorrectly");
+    if (rad <= 0) {
+        printf("Error: data entered incorrectly");
     }
-	return 0;
+    return 0;
 }
