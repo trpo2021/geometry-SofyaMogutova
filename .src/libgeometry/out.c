09ed50c6998073185bd3236a6a0a9edf4e4d3d3b
SofyaMogutova <smogutova1234@gmail.com>
2021-04-01 19:16:46 +0700

    Add a file out.c

--- .src/libgeometry/out.c
+++ .src/libgeometry/out.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "libgeometry/out.h"
+#include "out.h"
 
-void circle(int x,int y,double rad)
+void circle(double rad)
 {
 	float pi=3.14;
 	double perimetr,area;
 	perimetr=2*pi*rad;
-	area=pi*pow(rad,2);
+	area=pi*rad*rad;
 	printf("P=%.2lf\n",perimetr);
 	printf("S=%.2lf",area);
 }
