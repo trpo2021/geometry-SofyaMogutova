84dbc6dc847f6af5a39bd73d5da7d9bf89bef4d2
SofyaMogutova <smogutova1234@gmail.com>
2021-04-06 20:02:31 +0700

    Delete file

--- obj/src/geometry/geometry.d
+++
@@ -1,4 +0,0 @@
-obj/src/geometry/geometry.o: src/geometry/geometry.c \
- src/libgeometry/out.h
-
-src/libgeometry/out.h:
--- obj/src/libgeometry/out.d
+++
@@ -1,3 +0,0 @@
-obj/src/libgeometry/out.o: src/libgeometry/out.c src/libgeometry/out.h
-
-src/libgeometry/out.h:
